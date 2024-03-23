747f2b7eb5294e4a03e92a18f01c2bafb51d2967
Clinton Pikita <112552126+Clint07-datascientist@users.noreply.github.com>
2024-03-23 15:19:48 +0200

    Update 100-puppet_ssh_config.pp

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,12 @@
-#!/usr/bin/env bash
-# Using puppet to make changes to my configuration file
-
-
-file { 'etc/ssh/ssh_config':
-	ensure => present, 
-
-
-content =>"
-
-	#SSH client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PassswordAuthentication no
-
-} 
+# No PasswordAuthentication using puppet
+# SSH configuration file
+file { '/etc/ssh/ssh_config':
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
